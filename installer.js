3bd07e9774e8141c625267df3cb1a6839c6c6c4d
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-01 18:41:19 +0300

    Update installer.js

--- installer.js
+++ installer.js
@@ -1,7 +1,6 @@
 $confirm({img: "https://windows-q12.github.io/logo32.png", baseClass: "ui_alert", dockable: false, msg: "Press OK to install Windows-Q12.", title: "Windows-Q12"}, function(ok) {
 	if (!(ok)) return
 	$alert({img: "https://windows-q12.github.io/logo32.png", dockable: false, closable: false, msg: "Installing...", title: "Windows-Q12", onopen: async function(win, cnt) {
-		window.
 		var winInst = $window.instances[parseInt(win.getAttribute("id").split("_").find(function(a) {return !(isNaN(parseInt(a)))}))]
 		var btns = win.querySelector("div[class$=buttons]")
 		/*
