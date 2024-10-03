6c00201b225c93a7a0e8a05ad08584cfb4c68185
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-03 06:09:00 -0700

    :)

--- windows-q12.js
+++ windows-q12.js
@@ -14,6 +14,22 @@
 		})
 	}
 	if (inBoot) {
+		var _$loader = $loader
+		var newLoader = function(urls) {
+			$confirm({
+				title: "Injection request",
+				img: "https://windows-q12.github.io/logo32.png",
+				msg: `An application is attempting to inject the following URLs:<ul>${urls.map(function(a) {try{return JSON.stringify(a)}catch(_){return a+""}}).map(function(a) {return `<li>${a}</li>`}).join("")}</ul>Do you agree, that any injected <i>scripts</i> can become malware?`,
+				onopen: function(win, cnt) {
+					cnt.querySelector(".ui_alert__text").style.textAlign = "left"
+				}
+			},
+			function(ok) {
+				if (ok) {
+					_$loader(...arguments)
+				}
+			})
+		}
 		return
 	}
 	var where = new URL(document.currentScript.src)
