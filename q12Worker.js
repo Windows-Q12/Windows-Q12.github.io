84c0f3115929450b4d585986ecd2b2448ceabfa6
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-04 20:07:35 +0300

    Update q12Worker.js

--- q12Worker.js
+++ q12Worker.js
@@ -1,2 +1,6 @@
 if (self+"" != "[object ServiceWorkerGlobalScope]") throw new Error("who you think i am??")
+console.log("im runnig!!")
 
+self.addEventListener("fetch", function(e) {
+	console.log(e)
+})
