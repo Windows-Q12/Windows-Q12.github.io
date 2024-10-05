28696ba4a689a831c96b2d9e72080315d7467ce9
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-05 14:53:41 +0300

    .

--- windows-q12.js
+++ windows-q12.js
@@ -33,7 +33,8 @@
 		var _$loader = $loader
 		var newLoader = function(urls) {
 			var args = [...arguments]
-			var isAllowed = new Error().stack.split("\n").slice(1).map(function(a) {
+			var sUrls = urls.map(function(a){try{return JSON.stringify(a)}catch(_){return a+""}})
+			var isStackAllowed = new Error().stack.split("\n").slice(1).map(function(a) {
 				a = a.trim().split(" ").slice(1).join(" ") // remove the "at" in "at <location>"
 				return a
 			}).some(function(a) {
@@ -41,12 +42,14 @@
 					return a.includes(b)
 				})
 			})
-			if (isAllowed) return _$loader(...args)
+			var isSrcAllowed = sUrls.every(function(a,b){try{a=new URL(a)}catch(_){return false}a=a.href.slice(a.protocol.length).split("/");while(!a[0].length){a.shift()}return a.join("/").startsWith(loaderWhitelist.sources[b])})
+			// if (isStackAllowed) return _$loader(...args)
+			if ((isSrcAllowed) && (sUrls.every(function(a){return!(a.startsWith("blob:"))}))) return _$loader(...args)
 			if (!(document.getElementById("s42_desktop").querySelector("*"))) return _$loader(...args)
 			$confirm({
 				title: "Injection request",
 				img: "https://windows-q12.github.io/logo32.png",
-				msg: `An application is attempting to inject the following URLs:<ul>${urls.map(function(a) {try{return JSON.stringify(a)}catch(_){return a+""}}).map(function(a) {return `<li>${a}</li>`}).join("")}</ul>Do you agree, that any injected <i>scripts</i> can become malware?`,
+				msg: `An application is attempting to inject the following URLs:<ul>${sUrls.map(function(a){return`<li>${a}</li>`}).join("")}</ul>Do you agree, that any injected <i>scripts</i> can become malware?`,
 				onopen: function(win, cnt) {
 					cnt.querySelector(".ui_alert__text").style.textAlign = "left"
 				}
